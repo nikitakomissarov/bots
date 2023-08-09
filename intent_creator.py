907d192abbd884dfdb96dc2ac0d124df5af5008b
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2023-08-09 05:38:01 +0200

    вынес общие функции в отдельный фаль, починил цикл, включил fallback, отформатировал по pep8, перенес настройки логера

--- intent_creator.py
+++ intent_creator.py
@@ -1,47 +1,35 @@
-from main import logger_info, logger_error, handler, handler_format
 from google.cloud import dialogflow
 import json
 from dotenv import dotenv_values
 import logging
+from logger import TelegramLogsHandler, bot_logger
+from logging.handlers import TimedRotatingFileHandler
+from logger import TelegramLogsHandler, bot_logger
+
 
 config = dotenv_values('.env')
-
 TRAINING_PHRASES = config['TRAINING_PHRASES']
 GOOGLE_APPLICATION_CREDENTIALS = config['GOOGLE_APPLICATION_CREDENTIALS']
 
-with open(GOOGLE_APPLICATION_CREDENTIALS, "r") as google_file:
-  credentials = google_file.read()
-  credentials = json.loads(credentials)
+logger_info = logging.getLogger('loggerinfo')
+logger_error = logging.getLogger("loggererror")
 
-with open(TRAINING_PHRASES, "r",  encoding='utf-8') as phrases_file:
-  training_phrases_parts = phrases_file.read()
-  training_phrases_parts = json.loads(training_phrases_parts)
 
 def create_intent(project_id, training_phrases_parts):
+    for section in training_phrases_parts:
+        display_name = section
+        training_phrases_part = training_phrases_parts[display_name]['questions']
 
-    handler.setFormatter(handler_format)
-
-    logger_info.setLevel(logging.INFO)
-    logger_info.addHandler(handler)
-
-    logger_error.setLevel(logging.ERROR)
-    logger_error.addHandler(handler)
-
-    try:
-        for section in training_phrases_parts:
-            display_name = section
-
-        message_texts = []
-        message_texts.append(training_phrases_parts[display_name]['answer'])
+        message_texts = [training_phrases_parts[display_name]['answer']]
 
         intents_client = dialogflow.IntentsClient()
+
         parent = dialogflow.AgentsClient.agent_path(project_id)
 
         training_phrases = []
-        training_phrases_part = training_phrases_parts[display_name]['questions']
+        for a in training_phrases_part:
+            part = dialogflow.Intent.TrainingPhrase.Part(text=a)
 
-        for question in training_phrases_part:
-            part = dialogflow.Intent.TrainingPhrase.Part(text=question)
             training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
             training_phrases.append(training_phrase)
 
@@ -51,12 +39,41 @@ def create_intent(project_id, training_phrases_parts):
         intent = dialogflow.Intent(
             display_name=display_name, training_phrases=training_phrases, messages=[message]
         )
+        language_code = 'ru'
 
         response = intents_client.create_intent(
-            request={"parent": parent, "intent": intent}
+            request={"parent": parent, "intent": intent, "language_code": language_code}
         )
-        logger_info.info("Intent created: {}".format(response))
-    except Exception as err:
-        logger_error.exception(err)
 
-create_intent(credentials['quota_project_id'], training_phrases_parts)
+        logger_info.info("Intent created: {}".format(response))
+
+
+def main():
+    try:
+        handler = TimedRotatingFileHandler("app.log", when='D', interval=30, backupCount=1)
+        handler_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(handler_format)
+        logger_info.setLevel(logging.INFO)
+        logger_info.addHandler(handler)
+        logger_error.setLevel(logging.ERROR)
+        logger_error.addHandler(handler)
+        telegram_notification_handler = TelegramLogsHandler(bot_logger)
+        telegram_notification_handler.setFormatter(handler_format)
+        logger_error.addHandler(telegram_notification_handler)
+
+        with open(GOOGLE_APPLICATION_CREDENTIALS, "r") as my_file:
+            credentials = my_file.read()
+            credentials = json.loads(credentials)
+
+        with open(TRAINING_PHRASES, "r", encoding='utf-8') as phrases_file:
+            training_phrases_parts = phrases_file.read()
+            training_phrases_parts = json.loads(training_phrases_parts)
+
+        create_intent(credentials['quota_project_id'], training_phrases_parts)
+
+    except Exception as err:
+        logger_error.error(err, exc_info=True)
+
+
+if __name__ == '__main__':
+    main()
